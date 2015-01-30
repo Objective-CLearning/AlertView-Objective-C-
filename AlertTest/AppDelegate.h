cfa0e32fa68499cc40be4aedb4f5d087f1c92159
FM-C2 <fm-c2@FM-C2tekiiMac.local>
2015-01-30 13:41:35 +0800

    Initial Commit

---
+++ AlertTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertTest
+//
+//  Created by FM-C2 on 2015/1/30.
+//  Copyright (c) 2015年 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlertTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertTest
+//
+//  Created by FM-C2 on 2015/1/30.
+//  Copyright (c) 2015年 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
